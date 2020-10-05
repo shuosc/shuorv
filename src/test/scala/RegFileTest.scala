79881071ed68954e865e7e7898dd1dd9922eb6b9
龙方淞 <longfangsong@icloud.com>
2020-10-05 14:46:52 +0800

    add RegFile (#3)
    Signed-off-by: longfangsong <longfangsong@icloud.com>

---
+++ src/test/scala/RegFileTest.scala
@@ -0,0 +1,37 @@
+import chisel3.iotesters._
+import org.scalatest.{FlatSpec, Matchers}
+
+class RegFileTest(regFile: RegFile) extends PeekPokeTester(regFile) {
+  val cases = Array(
+    // addressA, addressB, addressC, input, writeEnable, expectedValueA, expectedValueB
+    (0, 0, 1, 1, true, 0, 0), // x1 = 1
+    (0, 0, 1, 2, true, 0, 0), // x1 = 2
+    (1, 0, 0, 0, false, 2, 0), // assert(x1 == 2)
+    (0, 0, 2, 1, true, 0, 0), // x2 = 1
+    (1, 2, 0, 0, false, 2, 1), // assert(x1 == 2, x2 == 1)
+  )
+  for ((addressA, addressB, addressC, input, writeEnable, expectedValueA, expectedValueB) <- cases) {
+    poke(regFile.io.addressA, addressA)
+    poke(regFile.io.addressB, addressB)
+    poke(regFile.io.addressInput, addressC)
+    poke(regFile.io.input, input)
+    poke(regFile.io.writeEnable, writeEnable)
+    step(1)
+    if (expectedValueA != 0) {
+      expect(regFile.io.outputA, expectedValueA)
+    }
+    if (expectedValueB != 0) {
+      expect(regFile.io.outputB, expectedValueB)
+    }
+  }
+}
+
+class RegFileSpec extends FlatSpec with Matchers {
+  behavior of "RegFileSpec"
+
+  it should "read and write successfully" in {
+    chisel3.iotesters.Driver(() => new RegFile) { regFile =>
+      new RegFileTest(regFile)
+    } should be(true)
+  }
+}
